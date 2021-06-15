1144b94b0303da9990b922ae25e6c486f24f53e4
Josh Fell <josh.d.fell@astronomer.io>
2021-06-14 22:23:09 -0400

    Upgrading image + update DAG

--- dags/certified-dag.py
+++ dags/certified-dag.py
@@ -1,13 +1,13 @@
-from airflow.decorators import dag, task
+from airflow.decorators import dag, task_group
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import get_current_context, PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.task_group import TaskGroup
 
 from datetime import datetime, timedelta
-from include import custom_function
+from include.custom_function import custom_function
 
-
+# Default settings applied to all tasks within the DAG.
 DEFAULT_ARGS = dict(
     owner="airflow",
     depends_on_past=False,
@@ -25,30 +25,45 @@ DAG_ARGS = dict(
     catchup=False,
 )
 
-
+# Using the `@dag` decorator, the `dag` argument doesn't need to be specified for each
+# task.  This functions the same way as the `with DAG()...` context manager.
 @dag(**DAG_ARGS)
 def certified_dag():
-    t0 = DummyOperator(task_id="start")
+    t_start = DummyOperator(task_id="start")
+    t_end = DummyOperator(task_id="end")
 
     with TaskGroup("group_bash_tasks") as group_bash_tasks:
-        bash_command = "sleep $[ ( $RANDOM % 30 )  + 1 ]s && date"
+        sleep = "sleep $[ ( $RANDOM % 30 )  + 1 ]s && date"
+
         t1 = BashOperator(
-            task_id="bash_print_date1",
-            bash_command=bash_command,
+            task_id="bash_begin",
+            bash_command="echo begin bash commands",
         )
         t2 = BashOperator(
             task_id="bash_print_date2",
-            bash_command=bash_command,
+            bash_command=sleep,
+        )
+        t3 = BashOperator(
+            task_id="bash_print_date3",
+            bash_command=sleep,
         )
 
+        # Lists can be used to specify tasks to execute in parallel.
+        t1 >> [t2, t3]
+
+    t_start >> group_bash_tasks >> t_end
+
+    # Generate tasks with a loop. The `task_id` must be unique across all tasks in a DAG.
     for task_number in range(5):
-        tn = task(task_id=f"python_print_date_{task_number}")(custom_function)(
-            task_number=task_number
+        tn = PythonOperator(
+            task_id=f"python_print_date_{task_number}",
+            python_callable=custom_function,
+            op_kwargs=dict(task_number=task_number),
         )
 
-        t0 >> tn
-
-    t0 >> group_bash_tasks
+        # When setting the dependencies, make sure they are indented inside the loop so
+        # each task is added downstream of `t_start`.
+        t_start >> tn >> t_end
 
 
 dag = certified_dag()
--- include/custom_function.py
+++ include/custom_function.py
@@ -1,10 +1,12 @@
-def custom_function(task_number, **kwargs):
+from datetime import datetime
+
+
+def custom_function(task_number: int, **context) -> None:
     """
     This can be any Python code you want and is called from the `PythonOperator`.
     The code is not executed until the task is run by the Airflow Scheduler.
     """
 
-    context = get_current_context()
     print(
         f"I am task number {task_number}."
         f"This DAG Run execution date is {context['ts']} and the current time is {datetime.now()}"
